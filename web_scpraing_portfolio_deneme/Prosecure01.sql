b04e364c2d09f0f7296eb82f26437063ee9de211
Abdullah Cay <abdullahcay26@gmail.com>
2023-03-19 20:50:13 +0100

    linkstogo added

--- web_scpraing_portfolio_deneme/Prosecure01.sql
+++ web_scpraing_portfolio_deneme/Prosecure01.sql
@@ -1,59 +1,23 @@
 BEGIN PROSECURE ;
 
--- 01. Find Daily datas 
-CREATE TABLE sildaily as 
-SELECT * FROM silall
-WHERE (Date_Part('day', date) = Extract(Day from Now())) AND (Date_Part('month', date) = Extract(MONTH from Now()))
-
--- 02. Add new datas to dailyhours
+-- 01. Add new datas to dailyhours
 INSERT INTO dailyhours
-SELECT * FROM dailyhours
-WHERE lower(publish) LIKE '%stunde%'; 
+SELECT * FROM alldata
+WHERE Date_Trunc('day', date) = CURRENT_DATE
+      AND lower(publish) LIKE '%stunde%'; 
 
--- 02. Add new datas to dailyhours
-INSERT INTO dailynew
-SELECT * FROM dailyshours
-WHERE lower(publish) LIKE '%stunde%';
+-- 02. Links to go (Data Engineer, Cloud Engineer)
 
--- 03. Clean 'daily' table
-TRUNCATE daily, dailyhours;
+INSERT INTO linkstogo
+SELECT id,CURRENT_DATE, job_title,company,link
+FROM 
+	(Select *, 
+		Row_Number() Over (Partition BY job_title,company ) as RN
+		From dailyhours) t1
+Where RN < 2 AND (lower(job_title) LIKE '%data engineer%' or lower(job_title) LIKE '%cloud%')
+             AND Date_Trunc('day', date) = CURRENT_DATE
+Order by 4,2
 
-CREATE TABLE dailyclean
 
 
--- 01. Find Daily datas 
-CREATE TABLE sildaily as 
-SELECT * FROM silall
-WHERE (Date_Part('day', date) = Extract(Day from Now())) AND (Date_Part('month', date) = Extract(MONTH from Now()))
 
-INSERT INTO silhours
-SELECT * FROM silall
-WHERE lower(publish) LIKE '%stunde%';
-
-CREATE TABLE silhours as
-SELECT * FROM sildaily
-WHERE lower(publish) LIKE '%stunde%'; 
-
-CREATE TABLE silhoursnoduplicate as
-SELECT * FROM silhours;
-
-DELETE FROM silhoursnoduplicate s
-    using
-    (
-        SELECT link,max(ctid) as max_ctid
-        FROM silhoursnoduplicate
-        GROUP BY link) t
-Where s.ctid <> t.max_ctid and s.link = t.link;
-----------------------------------
-With CTE as
-(
-	Select *, Row_Number() OVER (Partition by link ) as RN
-	from silhoursnoduplicate
-)
-Delete from CTE Where rn >1 ;
-
-SELECT * FROM silhoursnoduplicate
-
-SELECT * FROM silhours
-
-select id,job_title,publish,city,company,date,link from silhoursnoduplicate order by id
